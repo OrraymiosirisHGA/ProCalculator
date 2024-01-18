73bcc798bb1c22ee7c32137192d9297972ec3b96
ohgue <ohgue@TABLET-K283LJ13>
2024-01-17 22:24:42 -0500

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCalculator
+{
+    class Calculator
+    {
+        public static int sum(int x, int y)
+        {
+            int sresult = x + y;
+            return sresult;
+        }
+        public static int rest(int x, int y)
+        {
+            int sresult = (x) - (y);
+            return sresult;
+        }
+        public static int mult(int x, int y)
+        {
+            int sresult = x * y;
+            return sresult;
+        }
+        public static float div(float x, float y)
+        {
+            float sresult = x / y;
+            return sresult;
+        }
+        public static void Procalculator()
+        {
+            int select = 0;
+            int r1 = 0;
+            int r2 = 0;
+            int first = 0;
+            int second = 0;
+            Console.WriteLine("....................................................ProCalculator.......................................................");
+            Console.WriteLine("\nPlease Select from [1-4] the operation you want to do:");
+            do
+            {
+                try // It is used to catch errors or exceptions;  
+                {   //display a error message if the user do not enter one number from the menu.
+                    //Console.Clear();
+                    Console.WriteLine("[1]. Addition");
+                    Console.WriteLine("[2]. Substraction");
+                    Console.WriteLine("[3]. Multiplication");
+                    Console.WriteLine("[4]. Division");
+
+                    select = Int32.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("This is the wrong entrance please choose between [1-4].");
+                }
+            }
+            while (!(select == 1) && !(select == 2) && !(select == 3) && !(select == 4));//Condition that repeat 
+            // the menu over and over until the user enter the right number within the menu. 
+            switch (select)
+            {
+                case 1:
+                    //Console.Clear();
+                    Console.WriteLine("....................................................Addition......................................................");
+
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Please enter the 1st Number:");
+                            first = Int32.Parse(Console.ReadLine());
+                            r1 = first / first;//Help the program to identify if the number is an integer.
+                        }
+                        catch//catch the error or the exception from the user entering the wrong data.
+                        {
+                            Console.WriteLine("This is the wrong entrance please choose right #");
+                        }
+
+                    } while (!(r1 == 1));//keep the loop until the user enter a number and not something else.
+
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Please enter the 2nd Number:");
+                            second = Int32.Parse(Console.ReadLine());
+                            r2 = second / second;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("This is the wrong entrance please choose right #");
+                        }
+
+                    } while (!(r2 == 1));
+
+
+                    Console.WriteLine("The result of this {+} operation is: " + Calculator.sum(first, second));
+                    // call the function that add the 2 number entered by the user and print the result of the [+].
+                    break;
+
+                case 2:
+                    //Console.Clear();
+                    Console.WriteLine("....................................................Subtraction......................................................");
+
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Please enter the 1st Number:");
+                            first = Int32.Parse(Console.ReadLine());
+                            r1 = first / first;//Help the program to identify if the number is an integer.
+                        }
+                        catch//catch the error or the exception from the user entering the wrong data.
+                        {
+                            Console.WriteLine("This is the wrong entrance please choose right #");
+                        }
+
+                    } while (!(r1 == 1));//keep the loop until the user enter a number and not something else.
+
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Please enter the 2nd Number:");
+                            second = Int32.Parse(Console.ReadLine());
+                            r2 = second / second;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("This is the wrong entrance please choose right #");
+                        }
+
+                    } while (!(r2 == 1));
+
+
+                    Console.WriteLine("The result of this {-} operation is: " + Calculator.rest(first, second));
+                    // call the function that add the 2 number entered by the user and print the result of the [-].
+                    break;
+                case 3:
+                    //Console.Clear();
+                    Console.WriteLine("....................................................Multiplication......................................................");
+
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Please enter the 1st Number:");
+                            first = Int32.Parse(Console.ReadLine());
+                            r1 = first / first;//Help the program to identify if the number is an integer.
+                        }
+                        catch//catch the error or the exception from the user entering the wrong data.
+                        {
+                            Console.WriteLine("This is the wrong entrance please choose right #");
+                        }
+
+                    } while (!(r1 == 1));//keep the loop until the user enter a number and not something else.
+
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Please enter the 2nd Number:");
+                            second = Int32.Parse(Console.ReadLine());
+                            r2 = second / second;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("This is the wrong entrance please choose right #");
+                        }
+
+                    } while (!(r2 == 1));
+
+
+                    Console.WriteLine("The result of this {*} operation is: " + Calculator.mult(first, second));
+                    // call the function that add the 2 number entered by the user and print the result of the [*].
+                    break;
+                case 4:
+                    //Console.Clear();
+                    Console.WriteLine("....................................................Division......................................................");
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Please enter the 1st Number:");
+                            first = Int32.Parse(Console.ReadLine());
+                            r1 = first / first;//Help the program to identify if the number is an integer.
+                        }
+                        catch//catch the error or the exception from the user entering the wrong data.
+                        {
+                            Console.WriteLine("This is the wrong entrance please choose right #");
+                        }
+
+                    } while (!(r1 == 1));//keep the loop until the user enter a number and not something else.
+
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Please enter the 2nd Number:");
+                            second = Int32.Parse(Console.ReadLine());
+                            r2 = second / second;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("This is the wrong entrance please choose right #");
+                        }
+
+                    } while (!(r2 == 1));
+
+
+                    Console.WriteLine("The result of this {/} operation is: " + Calculator.div(first, second));
+                    // call the function that add the 2 number entered by the user and print the result of the [/].
+                    break;
+
+            }
+
+            //Console.ReadKey();
+
+        }
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Developer: Orraymiosiris H. Guevara.
+ * C# Visual Studio Console Calculator APP.
+ * Description: This program displays a MENU with the 4 basics operation 
+ * [+*-/]. It makes the user to choose a number from {1-4} and keeps asking over and over untit the user makes the right input.
+ * When the user chose the operation the calculator will prompt a message with the ongoing operation will ask for the 2 numbers
+ * to be calcalated depending on the operation already choose.
+ * The program consists of a Menu and  4 function for each operation.
+*/
+namespace ProCalculator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string q = "yes";
+           
+            do
+            {
+                try
+                {
+                    Console.Clear();
+                    Calculator.Procalculator();
+                    Console.WriteLine("\nType [yes] and then enter to go back to the main menu or any other key + enter to fully exit the program:");
+                    q = Console.ReadLine();
+                }
+                catch
+                {
+                    Console.WriteLine("Thank you for being an amazing user!");
+                }
+            } while ((q == "yes"));
+           
+            Console.WriteLine("Thank you for being an amazing user!");
+            Console.ReadKey(true);
+        }
+        
+    }
+
+}
